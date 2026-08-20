3cec7af199e9683df293d63e82048aecc3f27258
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-20 19:17:19 +0800

    Update scraper.h

--- src/scraper.h
+++ src/scraper.h
@@ -1,8 +1,9 @@
 #ifndef SCRAPER_H
 #define SCRAPER_H
 
-// Download cover art for a given serial/disc ID
 void scraper_download_cover(const char *serial);
 void scraper_download_gameindex(void);
+void scraper_force_download_cover(const char *serial);
+void scraper_force_download_gameindex(void);
 
 #endif
