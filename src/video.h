5a9e55f7775028ffead8417d451afe38570fcf55
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-20 19:12:03 +0800

    Add draw_image_rgba_fit function to video.h
    Added draw_image_rgba_fit function prototype.

--- src/video.h
+++ src/video.h
@@ -12,5 +12,7 @@ void draw_pixel(int x, int y, uint32_t color);
 void draw_rect(int x, int y, int w, int h, uint32_t color);
 void draw_rounded_rect(int x, int y, int w, int h, int radius, uint32_t color);
 void draw_image_rgba(int x, int y, int w, int h, const unsigned char *rgba, int img_w, int img_h);
+void draw_image_rgba_fit(int x, int y, int box_w, int box_h,
+                         const unsigned char *rgba, int img_w, int img_h);
 
 #endif
