e0b668b375ecd5bab1175469011f3ca6c235e05d
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-20 19:15:09 +0800

    Add config_get_game_emulator_info function prototype

--- src/config.h
+++ src/config.h
@@ -7,5 +7,7 @@
 
 int set_active_game(const char *iso_path, const char *disc_id,
                     const char *game_name, char *out_emulator_tid, size_t tid_size);
-
+int config_get_game_emulator_info(const char *disc_id, const char *game_name,
+                                   char *out_emu_name, size_t name_size,
+                                   char *out_emu_id, size_t id_size);
 #endif
