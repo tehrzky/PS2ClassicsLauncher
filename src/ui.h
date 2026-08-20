f5e0dc475da9aba8838a1c8c3d64af06a8c1fb38
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-20 19:18:37 +0800

    Add settings UI function and constants to ui.h

--- src/ui.h
+++ src/ui.h
@@ -1,6 +1,9 @@
 #ifndef UI_H
 #define UI_H
 
-void draw_launcher_ui(int game_count, int selected, int total_games);
+void draw_launcher_ui(int game_count_visible, int selected_idx, int total_games);
+void draw_settings_ui(int selected_item, int in_per_game);
+
+#define SETTINGS_ITEMS 6
 
 #endif
