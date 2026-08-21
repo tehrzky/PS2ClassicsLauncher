055801a4c0b6fa4c6b7da276b9eadda8ae61fb83
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-21 20:12:06 +0800

    Add functions to retrieve work and config paths
    Added functions to get paths for work and config files.

--- src/settings.c
+++ src/settings.c
@@ -16,6 +16,13 @@ void settings_reset(void) {
             "https://raw.githubusercontent.com/xlenore/ps2-covers/main",
             sizeof(g_settings.scraper_base_url) - 1);
     g_settings.scraper_base_url[sizeof(g_settings.scraper_base_url) - 1] = '\0';
+    g_settings.font_body = 0;
+    g_settings.font_title = 0;
+    strncpy(g_settings.work_path, "/data/PS4ROMS/PS2ISO", sizeof(g_settings.work_path) - 1);
+    g_settings.work_path[sizeof(g_settings.work_path) - 1] = '\0';
+    strncpy(g_settings.master_config, "config-emu-ex.txt", sizeof(g_settings.master_config) - 1);
+    g_settings.master_config[sizeof(g_settings.master_config) - 1] = '\0';
+    g_settings.wallpaper[0] = '\0';
 }
 
 void settings_load(void) {
@@ -55,4 +62,15 @@ void settings_save(void) {
     fprintf(fp, "cover_type=%d\n", g_settings.cover_type);
     fprintf(fp, "scraper_base_url=%s\n", g_settings.scraper_base_url);
     fclose(fp);
+}
+
+void settings_get_path(char *out, size_t out_len, const char *subpath) {
+    if (subpath && subpath[0])
+        snprintf(out, out_len, "%s/%s", g_settings.work_path, subpath);
+    else
+        strncpy(out, g_settings.work_path, out_len);
+}
+
+void settings_get_config_path(char *out, size_t out_len, const char *filename) {
+    snprintf(out, out_len, "%s/config/%s", g_settings.work_path, filename);
 }
