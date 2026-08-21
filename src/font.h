585e88bc8cacf6bede2a0f4d4ade786b6a737c92
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-21 12:42:32 +0800

    Refactor font.h to add font management functions

--- src/font.h
+++ src/font.h
@@ -3,10 +3,13 @@
 
 #include <stdint.h>
 
-#define FONT_WIDTH  8
-#define FONT_HEIGHT 8
+void font_init(void);
+void font_cleanup(void);
 
-void draw_char_scaled(int x, int y, char c, uint32_t color, int scale);
+void draw_text(int x, int y, const char *s, uint32_t color, int size_px);
 void draw_text_scaled(int x, int y, const char *s, uint32_t color, int scale);
+
+int font_text_width(const char *s, int size_px);
+int font_line_height(int size_px);
 
 #endif
