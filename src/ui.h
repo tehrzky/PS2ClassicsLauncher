50ad86bebc90794b5532a37e693f537ea8f28fe3
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-21 19:56:16 +0800

    Modify SETTINGS_ITEMS constant in ui.h
    Updated SETTINGS_ITEMS to 10.

--- src/ui.h
+++ src/ui.h
@@ -4,6 +4,6 @@
 void draw_launcher_ui(int game_count_visible, int selected_idx, int total_games);
 void draw_settings_ui(int selected_item, int in_per_game);
 
-#define SETTINGS_ITEMS 6
+#define SETTINGS_ITEMS 10
 
 #endif
