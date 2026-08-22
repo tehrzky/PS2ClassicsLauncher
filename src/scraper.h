4b86db9afb0349c8ce6689fd33ec3ccbbf39f7fe
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-22 16:17:53 +0800

    Add extern declarations for download status variables

--- src/scraper.h
+++ src/scraper.h
@@ -1,9 +1,12 @@
 #ifndef SCRAPER_H
 #define SCRAPER_H
 
+extern char g_download_status[128];
+extern int  g_download_active;
+
 void scraper_download_cover(const char *serial);
-void scraper_download_gameindex(void);
 void scraper_force_download_cover(const char *serial);
+void scraper_download_gameindex(void);
 void scraper_force_download_gameindex(void);
 
 #endif
