8ac5ade13a8261655637d8e40907a53c8482ed9b
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-22 11:39:21 +0800

    Refactor cover.h by removing includes and adding functions
    Removed unused includes and updated cover functions.

--- src/cover.h
+++ src/cover.h
@@ -1,15 +1,10 @@
 #ifndef COVER_H
 #define COVER_H
 
-#include <stdint.h>
-
-// Load a cover for a given serial/disc ID
 void cover_load(const char *serial);
-
-// Draw a cover at position (x, y) with size (w, h) for a given serial
-void cover_draw(int x, int y, int w, int h, const char *serial);
-
-// Free cover texture memory (call when exiting)
+void cover_draw_fit(int x, int y, int box_w, int box_h, const char *serial);
+void cover_draw_wallpaper(void);
+void cover_free_wallpaper(void);
 void cover_cleanup(void);
 
 #endif
