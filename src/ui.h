c84cb85f7972261b8825c57d878bc4447c7b1d2f
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-22 11:37:57 +0800

    Refactor ui.h to include stdint.h

--- src/ui.h
+++ src/ui.h
@@ -1,9 +1,11 @@
 #ifndef UI_H
 #define UI_H
 
-void draw_launcher_ui(int game_count_visible, int selected_idx, int total_games);
-void draw_settings_ui(int selected_item, int in_per_game);
+#include <stdint.h>
 
 #define SETTINGS_ITEMS 10
+
+void draw_launcher_ui(int game_count_visible, int selected_idx, int total_games);
+void draw_settings_ui(int selected_item, int in_per_game);
 
 #endif
